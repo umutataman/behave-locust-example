b5fd72e23e99205d45816a64706817d8c2103ffa
Umut Ataman <umutataman@gmail.com>
2022-01-05 13:07:15 +0300

    - API and Load tests updates - Fix for Cloudflare Checking browser issue

--- features/environment.py
+++ features/environment.py
@@ -4,9 +4,7 @@ import time
 
 import allure
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-from helpers import Web
+import undetected_chromedriver as uc
 
 
 def before_all(context):
@@ -14,16 +12,14 @@ def before_all(context):
         shutil.rmtree("capture_of_failed")
 
     # Global variables
-    context.environment = Web(context).get_environment()
-    context.app_url = context.config.userdata.get("app_url")
     context.headless = context.config.userdata.get("headless")
     # Global Delays
-    context.element_wait = 10
+    context.element_wait = 30
     context.ddos_attack_wait = 5
     context.search_wait = 4
     context.basket_after_load_wait = 2
 
-    print("Initials are done and test starting for " + context.environment)
+    print("Initials are done and test starting")
 
 
 def before_feature(context, feature):
@@ -31,37 +27,38 @@ def before_feature(context, feature):
 
 
 def before_scenario(context, scenario):
-    if context.headless == "true":
+    if "web" in scenario.effective_tags:
         options = webdriver.ChromeOptions()
         options.add_argument("start-maximized")
-        options.add_argument("--headless")
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option('useAutomationExtension', False)
         options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_argument("ignore-certificate-errors");
+        options.add_argument("ignore-certificate-errors")
         options.add_argument("--no-sandbox")
-        context.browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
-    else:
-        context.browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+
+        if context.headless == "true":
+            options.add_argument("--headless")
+            options.add_argument("--disable-gpu")
+
+        context.browser = uc.Chrome(chrome_options=options)
 
 
 def after_scenario(context, scenario):
-    if scenario.status == "failed":
-        ss_name = scenario.name + time.strftime("%H:%M:%S")
-        fail_path = "capture_of_failed" + "/" + time.strftime("%d%b") + "/" + context.ft_name
-        file_name = fail_path + "/" + ss_name + ".png"
+    if "web" in scenario.effective_tags:
+        if scenario.status == "failed":
+            ss_name = scenario.name + " - " + time.strftime("%H:%M:%S")
+            fail_path = "capture_of_failed" + "/" + time.strftime("%d%b") + "/" + context.ft_name
+            file_name = fail_path + "/" + ss_name + ".png"
 
-        # Capture for allure
-        allure.attach(context.browser.get_screenshot_as_png(),
-                      name=file_name,
-                      attachment_type=allure.attachment_type.PNG)
+            # Capture for allure
+            allure.attach(context.browser.get_screenshot_as_png(),
+                          name=file_name,
+                          attachment_type=allure.attachment_type.PNG)
 
-        # Capture for local
-        if not os.path.exists(fail_path):
-            os.makedirs(fail_path)
-        context.browser.save_screenshot(file_name)
+            # Capture for local
+            if not os.path.exists(fail_path):
+                os.makedirs(fail_path)
+            context.browser.save_screenshot(file_name)
 
-    context.browser.quit()
+        context.browser.quit()
 
 
 def after_all(context):
